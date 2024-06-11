2f1c0c0307ec41eb69937ffb8c2fdad14e1dc417
Chad <461755424@qq.com>
2024-06-11 17:31:30 +1200

    basic-practice

---
+++ myapp/src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Description
+ * @authors 23/Chad Chad33009@gmail.com
+ * @date    2024-06-08 18:31:10
+ * @version 1.0.0
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.createRoot("11111", document.getElementById('root'))
